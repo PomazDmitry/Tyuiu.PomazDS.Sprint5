41768eccaf036c3543224b898dff7cc026dbe234
admin <zed-n.v@mail.ru>
2023-11-17 12:35:30 +0500

    Add project files.

---
+++ Tyuiu.PomazDS.Sprint5.Task0.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PomazDS.Sprint5.Task0.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PomazDS.Sprint5.Task0.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4e691d86-c6e5-476a-b986-c25b475ca216")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PomazDS.Sprint5.Task0.V3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PomazDS.Sprint5.Task0.V3.Lib;
+using autofill_pattern_lib;
+using System.IO;
+
+namespace Tyuiu.PomazDS.Sprint5.Task0.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            AutoFill ptrn = new AutoFill();
+
+            ptrn.MainPattern(5, "Класс File. Запись данных в текстовый файл", 0, 3, "Дано выражение вычислить его значение при x = 3, результат сохранить в текстовый файл OutPutFileTask0.txt и вывести на консоль. Округлить до трёх знаков после запятой.", "x=3. Формула: -(1.0 / 4) * (x^3 - 3 * x^2 + 4)");
+
+            int x = 3;
+
+            ptrn.ResultPattern();
+
+            Console.WriteLine("Путь: {0}\nРезультат: {1}",ds.SaveToFileTextData(x),File.ReadAllText($@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt"));
+        }
+    }
+}
